226466e83fcca29dc654175d2a62f277405c1f45
Joel Rafael Paredes Brioso <maria.brioso.joel@gmail.com>
2023-07-23 12:54:02 -0400

    Se agrego nuevos cambios en terrenos

--- src/app/models/poligonos/terrenos19_model.js
+++ src/app/models/poligonos/terrenos19_model.js
@@ -3,7 +3,19 @@ const sequelize = require("../../database/conexion");
 
 const land19 = async () => {
   const [result, metadata] = await sequelize.query(
-    "SELECT * FROM catastro.terrenos19 LIMIT 500"
+    `SELECT id, 
+           codigo, 
+           agua, 
+           alcantarillado, 
+           barrio, 
+           base, 
+           direccion, 
+           energia, 
+           este, 
+           fondo,
+           frente,
+           geom
+           FROM catastro.terrenos19 LIMIT 500`
   );
   return result;
 };
