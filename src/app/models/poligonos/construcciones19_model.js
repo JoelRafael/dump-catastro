b3db80809e18c2309d81ff763ec02ba4fc3f36ad
Joel Rafael Paredes Brioso <maria.brioso.joel@gmail.com>
2023-08-30 10:47:38 -0400

    Se agrego una nueva estructura para la busquedad

--- src/app/models/poligonos/construcciones19_model.js
+++ src/app/models/poligonos/construcciones19_model.js
@@ -32,32 +32,35 @@ const building19 = async () => {
 
 const getByCodigo = async (value) => {
   const [result, metadata] = await sequelize.query(
-    `SELECT c.id,
-            c.aire,
-            c.anyo,
-            c.ascensores,
-            c.calefaccion,
-            c.cod,
-            c.dormitorios,
-            c.escalera,
-            c.geom,
-            c.lavanderia,
-            c.plantas,
-            c.sanitarios,
-            c.servicio,
-            c.tanque,
-            co.id idConservacion,
-            co.estado estadoConservacion,
-            co.valor Conservacion,
-            r.id idrevestimiento,
-            r.revestimiento
+    `SELECT 
+            JSONB_BUILD_OBJECT(
+              'id',                  c.id,
+              'aire',                c.aire,
+              'anyo',                c.anyo,
+              'ascensores',          c.ascensores,
+              'calefaccion',         c.calefaccion,
+              'cod',                 c.cod,
+              'dormitorios',         c.dormitorios,
+              'escalera',            c.escalera,
+              'lavanderia',          c.lavanderia,
+              'plantas',             c.plantas,
+              'sanitarios',          c.sanitarios,
+              'servicio',            c.servicio,
+              'tanque',              c.tanque,
+              'idConservacion',      co.id ,
+              'estadoConservacion',  co.estado ,
+              'Conservacion',        co.valor ,
+              'idrevestimiento',     r.id ,
+              'revestimiento',       r.revestimiento
+                 ) properties,
+                 'Feature' as type,
+            c.geom geometry     
             FROM catastro.construcciones19 c 
             LEFT JOIN catastro.conservacion co ON c.conservacion = co.id
             LEFT JOIN catastro.revestimiento r ON c.revestimiento = r.id
             WHERE c.codigo IN(:value)`,
     {
-      replacements: { value: value }
-      
+      replacements: { value: value },
     }
   );
   return result;
--- src/app/models/poligonos/terrenos19_model.js
+++ src/app/models/poligonos/terrenos19_model.js
@@ -45,48 +45,46 @@ const land19 = async () => {
           LEFT JOIN catastro.material_via m ON m.id = t.material_via
           LEFT JOIN catastro.tipo_via tv ON tv.id = t.via
           LEFT JOIN catastro.caracter_titular c ON c.id = t.via
-          LIMIT 500
-          `
+          LIMIT 500`
   );
   return result;
 };
 const getbyCatastro = async (value) => {
   const [result, metadata] = await sequelize.query(
-    `SELECT t.id, 
-          t.codigo, 
-          t.agua, 
-          t.alcantarillado, 
-          t.barrio, 
-          t.base, 
-          t.direccion, 
-          t.energia, 
-          t.este, 
-          t.fondo,
-          t.frente,
-          t.internet,
-          t.manzano,
-          t.norte,
-          t.oeste,
-          t.predio,
-          t.subpredio,
-          t.superficie,
-          t.suptest,
-          t.sur,
-          t.telefono,
-          t.transporte,
-          ti.nombre,
-          ti.apellidos,
-          ti.documento,
-          ad.adquisicion,
-          ad.codigo codigoadquisicion,
-          f.forma,
-          f.valor,
-          f.codigo codigoforma,
-          m.material,
-          m.valor,
-          tv.tipo,
-          tv.valor valortipovia,
-          t.geom
+    `SELECT  JSONB_BUILD_OBJECT('valortipovia', tv.valor,
+                           'tipo',tv.tipo,
+                           'valor',m.valor,
+                           'material', m.material,
+                           'codigoforma', f.codigo,
+                           'adquisicion', ad.adquisicion,
+                           'documento', ti.documento,
+                           'apellidos',ti.apellidos,
+                           'nombre',ti.nombre,
+                           'transporte',t.transporte,
+                           'telefono',t.telefono,
+                           'sur',t.sur,
+                           'suptest',t.suptest,
+                           'superficie',t.superficie,
+                           'subpredio',t.subpredio,
+                           'predio',t.predio,
+                           'id',t.id,
+                           'codigo',t.codigo, 
+                           'agua',t.agua, 
+                           'alcantarillado',t.alcantarillado, 
+                           'barrio',t.barrio, 
+                           'base',t.base, 
+                           'direccion',t.direccion, 
+                           'energia',t.energia, 
+                           'este',t.este, 
+                           'fondo',t.fondo,
+                           'frente',t.frente,
+                           'internet',t.internet,
+                           'manzano',t.manzano,
+                           'norte',t.norte,
+                           'oeste',t.oeste
+                          ) properties,
+                          'Feature' as type,
+                          t.geom geometry
           FROM catastro.terrenos19 t 
           JOIN catastro.titular ti ON t.titular = ti.id
           JOIN catastro.adquisicion ad ON ad.id = ti.adquisicion
@@ -95,49 +93,49 @@ const getbyCatastro = async (value) => {
           LEFT JOIN catastro.tipo_via tv ON tv.id = t.via
           LEFT JOIN catastro.caracter_titular c ON c.id = t.via
           WHERE t.codigo ='${value}' 
-          OR ti.nombre LIKE '%${value}%'
-         
-          `
+          OR ti.nombre LIKE '%${value}%'`,
+    {
+      nest: false,
+    }
   );
   return result;
 };
 const getbyDocument = async (value) => {
   const [result, metadata] = await sequelize.query(
-    `SELECT t.id, 
-          t.codigo, 
-          t.agua, 
-          t.alcantarillado, 
-          t.barrio, 
-          t.base, 
-          t.direccion, 
-          t.energia, 
-          t.este, 
-          t.fondo,
-          t.frente,
-          t.internet,
-          t.manzano,
-          t.norte,
-          t.oeste,
-          t.predio,
-          t.subpredio,
-          t.superficie,
-          t.suptest,
-          t.sur,
-          t.telefono,
-          t.transporte,
-          ti.nombre,
-          ti.apellidos,
-          ti.documento,
-          ad.adquisicion,
-          ad.codigo codigoadquisicion,
-          f.forma,
-          f.valor,
-          f.codigo codigoforma,
-          m.material,
-          m.valor,
-          tv.tipo,
-          tv.valor valortipovia,
-          t.geom
+    `SELECT  JSONB_BUILD_OBJECT('valortipovia', tv.valor,
+                           'tipo',tv.tipo,
+                           'valor',m.valor,
+                           'material', m.material,
+                           'codigoforma', f.codigo,
+                           'adquisicion', ad.adquisicion,
+                           'documento', ti.documento,
+                           'apellidos',ti.apellidos,
+                           'nombre',ti.nombre,
+                           'transporte',t.transporte,
+                           'telefono',t.telefono,
+                           'sur',t.sur,
+                           'suptest',t.suptest,
+                           'superficie',t.superficie,
+                           'subpredio',t.subpredio,
+                           'predio',t.predio,
+                           'id',t.id,
+                           'codigo',t.codigo, 
+                           'agua',t.agua, 
+                           'alcantarillado',t.alcantarillado, 
+                           'barrio',t.barrio, 
+                           'base',t.base, 
+                           'direccion',t.direccion, 
+                           'energia',t.energia, 
+                           'este',t.este, 
+                           'fondo',t.fondo,
+                           'frente',t.frente,
+                           'internet',t.internet,
+                           'manzano',t.manzano,
+                           'norte',t.norte,
+                           'oeste',t.oeste
+                          ) properties,
+                          'Feature' as type,
+                          t.geom geometry
           FROM catastro.terrenos19 t 
           JOIN catastro.titular ti ON t.titular = ti.id
           JOIN catastro.adquisicion ad ON ad.id = ti.adquisicion
@@ -146,8 +144,10 @@ const getbyDocument = async (value) => {
           LEFT JOIN catastro.tipo_via tv ON tv.id = t.via
           LEFT JOIN catastro.caracter_titular c ON c.id = t.via
           WHERE ti.documento = ${value} 
-         
-          `
+          `,
+    {
+      nest: true,
+    }
   );
   return result;
 };
